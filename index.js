f6e266170a37357b213bdf04696c3041750db247
Mike Chevett <mchevett@gmail.com>
2014-12-23 09:11:28 -0500

    start adding childdomains and forms

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.setting('couch-base-url', 'http://ec2-54-84-90-63.compute-1.amazonaws.com:59
 app.setting('couch-username', 'ro');
 
 var activityService = {
-	getBeginTime: function(d){ return d.beginTime; },
+	getBeginTime: function(d){ return d.beginTime || d.timestamp; },
 	getEndTime: function(d){ return d.endTime; },
 	start: function(d){ d.beginTime = new Date(); },
 	stop: function(d){ d.endTime = new Date(); },
@@ -14,11 +14,6 @@ var activityService = {
 	},
 };
 
-var eventService = {
-	getTimestamp: function(d) { return d.timestamp; },
-	create: function(d){ d.timestamp = new Date(); },
-	locationUpdate: function(d, l){ d.location = l; },
-};
 
 var observerActivity = app.createDomain({name:'observer-activity', label: 'Observer Activity'});
 observerActivity.register('form-fields', require('./forms/observer-activity.json'));
@@ -28,20 +23,39 @@ observerActivity.register('short-description', function(d){
 });
 observerActivity.register('child-domains', ['follow', 'sighting']);
 
+// var environment = app.createDomain({name: 'environment', label: 'Environment'});
+// environment.register('form-fields', require('./forms/environment.json'));
+// environment.register('activity', activityService);
+// environment.register('short-description', 'title');
 
-var environment = app.createDomain({name: 'environment', label: 'Environment'});
-environment.register('form-fields', require('./forms/environment.json'));
-environment.register('activity', activityService);
-environment.register('short-description', 'title');
+// var follow = app.createDomain({name: 'follow', label: 'Follow'});
+// follow.register('form-fields', require('./forms/follow.json'));
+// follow.register('activity', activityService);
+// follow.register('short-description', 'title');
 
-var follow = app.createDomain({name: 'follow', label: 'Follow'});
-follow.register('form-fields', require('./forms/follow.json'));
-follow.register('activity', activityService);
-follow.register('short-description', 'title');
+
+
+var aggressionEvent = app.createDomain({name: 'aggression-event', label:'Aggression'});
+aggressionEvent.register('form-fields', require('./forms/aggression-event.json'));
+aggressionEvent.register('activity', activityService);
+aggressionEvent.register('short-description', function(d){
+	return 'Agreession towards - ' + d.animal;
+});
+
+var focal = app.createDomain({name: 'focal', label: 'Focal'});
+focal.register('form-fields', require('./forms/focal.json'));
+focal.register('activity', activityService);
+focal.register('short-description', function(d){
+	return 'Focal - ' + d.animal;
+});
+focal.register(aggressionEvent);
 
 var sighting = app.createDomain({name: 'sighting', label: 'Sighting'});
 sighting.register('form-fields', require('./forms/sighting.json'));
-sighting.register('event', eventService);
-sighting.register('short-description', 'title');
+sighting.register('activity', activityService);
+sighting.register('short-description', function(d){
+	return 'Sighting - ' + (d.title || d.sightingName);
+});
+sighting.register(focal);
 
 module.exports = app.getRegistry();
