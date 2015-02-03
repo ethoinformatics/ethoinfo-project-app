74acb2b6f84410f4153616c49a6330c77801a5b4
Mike Chevett <mchevett@gmail.com>
2015-02-03 01:06:32 -0500

    use domains from tony

--- index.js
+++ index.js
@@ -14,42 +14,46 @@ var activityService = {
 	},
 };
 
+// ****************************************************************************
+// * CONTACT                                                                  *
+// ****************************************************************************
+var contact = app.createDomain({name: 'contact', label: 'Contact'});
+contact.register('form-fields', require('./forms/contact.json'));
+contact.register('activity', activityService);
+contact.register('long-description', function(d){
+	var h1 = 'Contact with ' + this.getDescription('taxon') + ' (' +this.getDescription('group')+ ')';
+	var div = d.notes;
 
+	return '<h1>'+h1+'</h1>' + 
+		'<div style="font-style:italic;">' + div + '</div>';
+});
+
+contact.register('short-description', function(d){
+	return 'Sighting - ' + (d.title || d.sightingName);
+});
+
+// ****************************************************************************
+// * OBSERVER ACTIVITY                                                        *
+// ****************************************************************************
 var observerActivity = app.createDomain({name:'observer-activity', label: 'Observer Activity'});
 observerActivity.register('form-fields', require('./forms/observer-activity.json'));
 observerActivity.register('activity', activityService);
 observerActivity.register('short-description', function(d){
 	return 'observer - ' + d.title;
 });
-observerActivity.register('child-domains', ['follow', 'sighting']);
 
-var aggressionEvent = app.createDomain({name: 'aggression-event', label:'Aggression'});
-aggressionEvent.register('form-fields', require('./forms/aggression-event.json'));
-aggressionEvent.register('activity', activityService);
-aggressionEvent.register('long-description', function(d){
-	var h1 = 'Agression towards ' + this.getDescription('animal');
-	var h2 = this.getDescription('age') + ' ' + this.getDescription('sex');
-	var div = d.notes;
 
-	return '<h1>'+h1+'</h1>' + 
-		'<h3>' + h2 + '</h3>' + 
-		'<div style="font-style:italic;">' + div + '</div>';
-});
-aggressionEvent.register('short-description', function(d){
-	return 'Agression towards - ' + d.animal;
-});
+// var outOfViewState = app.createDomain({name: 'out-of-view-state', label:'Out of view'});
+// outOfViewState.register('form-fields', require('./forms/out-of-view-state.json'));
+// outOfViewState.register('activity', activityService);
+// outOfViewState.register('short-description', function(d){
+// 	return 'Out of view: ' + d.state;
+// });
 
-var outOfViewState = app.createDomain({name: 'out-of-view-state', label:'Out of view'});
-outOfViewState.register('form-fields', require('./forms/out-of-view-state.json'));
-outOfViewState.register('activity', activityService);
-outOfViewState.register('short-description', function(d){
-	return 'Out of view: ' + d.state;
-});
-
-var focal = app.createDomain({name: 'focal', label: 'Focal'});
-focal.register('form-fields', require('./forms/focal.json'));
-focal.register('activity', activityService);
-focal.register('long-description', function(d){
+var focalSample = app.createDomain({name: 'focal', label: 'Focal'});
+focalSample.register('form-fields', require('./forms/focal-sample.json'));
+focalSample.register('activity', activityService);
+focalSample.register('long-description', function(d){
 	var h1 = 'Focal of ' + this.getDescription('animal');
 	var h2 = this.getDescription('age') + ' ' + this.getDescription('sex');
 	var div = d.notes;
@@ -59,56 +63,124 @@ focal.register('long-description', function(d){
 		'<div style="font-style:italic;">' + div + '</div>';
 });
 
-focal.register('short-description', function(d){
+focalSample.register('short-description', function(d){
 	return 'Focal - ' + (d.title || d.notes);
 });
-focal.register(aggressionEvent);
-focal.register(outOfViewState);
 
-var sighting = app.createDomain({name: 'sighting', label: 'Sighting'});
-sighting.register('form-fields', require('./forms/sighting.json'));
-sighting.register('activity', activityService);
-sighting.register('long-description', function(d){
-	var h1 = 'Sighting of ' + this.getDescription('taxon') + ' (' +this.getDescription('group')+ ')';
+
+// var feedingBout = app.createDomain({name: 'feeding-bout', label: 'Feeding Bout'});
+// feedingBout.register('form-fields', require('./forms/placeholder.json'));
+// feedingBout.register('activity', activityService);
+// feedingBout.register('long-description', function(d){
+// 	var h1 = 'Feeding bout';
+// 	var h2 = d.placeholder;
+
+// 	return '<h1>'+h1+'</h1>' + 
+// 		'<h3>' + h2 + '</h3>';
+// });
+
+// feedingBout.register('short-description', function(d){
+// 	return 'Focal - ' + (d.title || d.notes);
+// });
+
+// ****************************************************************************
+// * FOCAL BEHAVIOR                                                           *
+// ****************************************************************************
+var focalBehavior = app.createDomain({name: 'focal-behavior', label:'Behavior'});
+focalBehavior.register('form-fields', require('./forms/focal-behavior.json'));
+focalBehavior.register('activity', activityService);
+focalBehavior.register('long-description', function(d){
+	var h1 = 'Aggression towards ' + this.getDescription('animal');
+	var h2 = this.getDescription('age') + ' ' + this.getDescription('sex');
 	var div = d.notes;
 
 	return '<h1>'+h1+'</h1>' + 
+		'<h3>' + h2 + '</h3>' + 
 		'<div style="font-style:italic;">' + div + '</div>';
 });
 
-sighting.register('short-description', function(d){
-	return 'Sighting - ' + (d.title || d.sightingName);
-});
-sighting.register(focal);
+// ****************************************************************************
+// * SOCIAL FOCAL BEHAVIOR                                                           *
+// ****************************************************************************
+var socialFocalBehavior = app.createDomain({name: 'social-focal-behavior', label:'Social behavior'});
+socialFocalBehavior.register('form-fields', require('./forms/social-focal-behavior.json'));
+socialFocalBehavior.register('activity', activityService);
+socialFocalBehavior.register('long-description', function(d){
+	var h1 = this.getDescription('type') + ' towards ' + this.getDescription('animal');
+	var h2 = this.getDescription('age') + ' ' + this.getDescription('sex');
+	var div = d.notes;
 
-var animalGroup = app.createDomain({name: 'animal-group', label: 'Animal Group'});
-animalGroup.register('code-domain', true);
-animalGroup.register('form-fields', require('./forms/animal-group.json'));
-animalGroup.register('short-description', function(d){ return d.name; });
+	return '<h1>'+h1+'</h1>' + 
+		'<h3>' + h2 + '</h3>' + 
+		'<div style="font-style:italic;">' + div + '</div>';
+});
+
+
+// ****************************************************************************
+// * POOP SAMPLE                                                              *
+// ****************************************************************************
+var poopSample = app.createDomain({name: 'poop-sample', label:'Poop sample'});
+poopSample.register('form-fields', require('./forms/poop-sample.json'));
+poopSample.register('activity', activityService);
+poopSample.register('long-description', function(){
+	var h1 = 'Poop sample from ' + this.getDescription('animal');
+	var h2 = '';
+	var div = '';
+
+	return '<h1>'+h1+'</h1>' + 
+		'<h3>' + h2 + '</h3>' + 
+		'<div style="font-style:italic;">' + div + '</div>';
+});
+
+
+// ****************************************************************************
+// * CODES                                                                    *
+// ****************************************************************************
+function createSimpleCodeDomain(name, label){
+	var domain = app.createDomain({name: name, label: label});
+	domain.register('code-domain', true);
+	domain.register('form-fields', [ { fields: { name: { type: 'text' } } } ]);
+	domain.register('short-description', function(d){ return d.name; });
+
+	return domain;
+}
+
+createSimpleCodeDomain('animal-group', 'Animal Group');
+createSimpleCodeDomain('user', 'User');
+createSimpleCodeDomain('taxon', 'Taxon');
+createSimpleCodeDomain('age-class', 'Age class');
+createSimpleCodeDomain('sex', 'Sex');
+createSimpleCodeDomain('focal-behavior-type', 'Behavior type');
+createSimpleCodeDomain('social-focal-behavior-type', 'Social behavior type');
 
 var animal = app.createDomain({name: 'animal', label: 'Animal'});
 animal.register('code-domain', true);
 animal.register('form-fields', require('./forms/animal.json'));
 animal.register('short-description', function(d){ return d.name; });
 
-var taxon = app.createDomain({name: 'taxon', label: 'Taxon'});
-taxon.register('code-domain', true);
-taxon.register('form-fields', require('./forms/taxon.json'));
-taxon.register('short-description', function(d){ return d.name; });
 
-var user = app.createDomain({name: 'user', label: 'User'});
-user.register('code-domain', true);
-user.register('form-fields', require('./forms/user.json'));
-user.register('short-description', function(d){ return d.name; });
 
-var ageClass = app.createDomain({name: 'age-class', label: 'Age class'});
-ageClass.register('code-domain', true);
-ageClass.register('form-fields', require('./forms/age-class.json'));
-ageClass.register('short-description', function(d){ return d.name; });
+// ****************************************************************************
+// * SET DOMAIN RELATIONSHIPS                                                 *
+// ****************************************************************************
 
-var sex = app.createDomain({name: 'sex', label: 'Sex'});
-sex.register('code-domain', true);
-sex.register('form-fields', require('./forms/sex.json'));
-sex.register('short-description', function(d){ return d.name; });
+
+// contact.register(focalSample, 'groupCompostions');
+// contact.register(focalSample, 'rollCallCensuses');
+// contact.register(focalSample, 'experiments');
+// contact.register(feedingBout, 'feedingBouts');
+// contact.register(focalSample, 'groupScans');
+// contact.register(focalSample, 'focalSamples');
+// contact.register(focal, 'processing');
+// contact.register(focal, 'resourcePatches');
+// focalSample.register(feedingBout, 'feedingBouts');
+// focalSample.register(feedingBout, 'feedingBouts');
+
+contact.register('focalSamples', focalSample);
+contact.register('collections', poopSample);
+
+focalSample.register('behaviors', socialFocalBehavior);
+focalSample.register('behaviors', focalBehavior);
+focalSample.register('collections', poopSample);
 
 module.exports = app.getRegistry();
